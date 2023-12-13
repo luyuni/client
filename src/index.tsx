d43e070eb7f68951334fdda57984aa30243335a2
luyuni <niyulu695@163.com>
2023-12-14 00:46:43 +0800

    init react

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './pages/Hello';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello></Hello>
   </React.StrictMode>
 );
 
